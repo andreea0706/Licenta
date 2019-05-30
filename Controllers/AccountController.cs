850381efcd3576330d76ba7e06b61d344df8ca1d
Andreea Ionescu <andreea.ionescu07@yahoo.com>
2019-05-31 00:58:50 +0300

    test commit

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860 blabla
 
 namespace FacturaOnline.Controllers
 {
